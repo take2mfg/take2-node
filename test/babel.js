0134d4133f7c23518609d53c7e16c1a4b8df2610
Ty Barho <t.barho@gmail.com>
2015-10-16 17:05:22 -0500

    strapping up a resource

---
+++ test/babel.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var chai           = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+var chaiString     = require("chai-string");
+
+chai.should();
+chai.use(chaiAsPromised);
+chai.use(chaiString);
+
+global.chaiAsPromised = chaiAsPromised;
+global.expect         = chai.expect;
+global.assert         = chai.assert;
+global.TEST_AUTH_KEY  = 'xxxyyyyyyyyyyyyyyyyyyy';
+
+var options = {};
+require("babel/register")(options);
