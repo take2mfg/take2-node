9c23900c23fdb4d53a3efd27bf935c06082e95ae
Ty Barho <t.barho@gmail.com>
2015-11-10 17:42:20 -0600

    only 1 method on visitor token, parse json response

--- test/resources/VisitorToken.spec.js
+++ test/resources/VisitorToken.spec.js
@@ -5,33 +5,33 @@ const Resource = 'VisitorToken';
 
 describe(`${Resource} Resource`, function() {
 
-  describe('list', function() {
-    it('sends the correct request', function() {
-      take2.visitorTokens.list();
-      expect(take2.LAST_REQUEST).to.deep.equal({
-        method: 'GET',
-        url: 'http://api.take2.co:443/v1/visitorTokens',
-        headers: {
-          'Authorization' : `Basic ${TEST_AUTH_KEY}`,
-          'Content-Type'  : 'application/json'
-        }
-      });
-    });
-  });
+  // describe('list', function() {
+  //   it('sends the correct request', function() {
+  //     take2.visitorTokens.list();
+  //     expect(take2.LAST_REQUEST).to.deep.equal({
+  //       method: 'GET',
+  //       url: 'http://api.take2.co:443/v1/visitorTokens',
+  //       headers: {
+  //         'Authorization' : `Basic ${TEST_AUTH_KEY}`,
+  //         'Content-Type'  : 'application/json'
+  //       }
+  //     });
+  //   });
+  // });
 
-  describe('retrieve', function() {
-    it('sends the correct request', function() {
-      take2.visitorTokens.retrieve(1);
-      expect(take2.LAST_REQUEST).to.deep.equal({
-        method: 'GET',
-        url: 'http://api.take2.co:443/v1/visitorTokens/1',
-        headers: {
-          'Authorization' : `Basic ${TEST_AUTH_KEY}`,
-          'Content-Type'  : 'application/json'
-        }
-      });
-    });
-  });
+  // describe('retrieve', function() {
+  //   it('sends the correct request', function() {
+  //     take2.visitorTokens.retrieve(1);
+  //     expect(take2.LAST_REQUEST).to.deep.equal({
+  //       method: 'GET',
+  //       url: 'http://api.take2.co:443/v1/visitorTokens/1',
+  //       headers: {
+  //         'Authorization' : `Basic ${TEST_AUTH_KEY}`,
+  //         'Content-Type'  : 'application/json'
+  //       }
+  //     });
+  //   });
+  // });
 
   describe('create', function() {
     it('sends the correct request', function() {
@@ -48,33 +48,33 @@ describe(`${Resource} Resource`, function() {
     });
   });
 
-  describe('update', function() {
-    it('sends the correct request', function() {
-      take2.visitorTokens.update(1, {});
-      expect(take2.LAST_REQUEST).to.deep.equal({
-        method: 'PUT',
-        url: 'http://api.take2.co:443/v1/visitorTokens/1',
-        headers: {
-          'Authorization' : `Basic ${TEST_AUTH_KEY}`,
-          'Content-Type'  : 'application/json'
-        },
-        json: {}
-      });
-    });
-  });
+  // describe('update', function() {
+  //   it('sends the correct request', function() {
+  //     take2.visitorTokens.update(1, {});
+  //     expect(take2.LAST_REQUEST).to.deep.equal({
+  //       method: 'PUT',
+  //       url: 'http://api.take2.co:443/v1/visitorTokens/1',
+  //       headers: {
+  //         'Authorization' : `Basic ${TEST_AUTH_KEY}`,
+  //         'Content-Type'  : 'application/json'
+  //       },
+  //       json: {}
+  //     });
+  //   });
+  // });
 
-  describe('destroy', function() {
-    it('sends the correct request', function() {
-      take2.visitorTokens.destroy(1);
-      expect(take2.LAST_REQUEST).to.deep.equal({
-        method: 'DELETE',
-        url: 'http://api.take2.co:443/v1/visitorTokens/1',
-        headers: {
-          'Authorization' : `Basic ${TEST_AUTH_KEY}`,
-          'Content-Type'  : 'application/json'
-        }
-      });
-    });
-  });
+  // describe('destroy', function() {
+  //   it('sends the correct request', function() {
+  //     take2.visitorTokens.destroy(1);
+  //     expect(take2.LAST_REQUEST).to.deep.equal({
+  //       method: 'DELETE',
+  //       url: 'http://api.take2.co:443/v1/visitorTokens/1',
+  //       headers: {
+  //         'Authorization' : `Basic ${TEST_AUTH_KEY}`,
+  //         'Content-Type'  : 'application/json'
+  //       }
+  //     });
+  //   });
+  // });
 
 });
